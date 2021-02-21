1ed942b8760803f70bdf2cc40ae529e048c3b583
kriskrok <25787775+kriskrok@users.noreply.github.com>
2021-02-21 22:36:34 +0200

    Adds first draft of login functionality
    - tempolates/login_form.html needs some further attention

--- application/auth/models.py
+++ application/auth/models.py
@@ -6,11 +6,23 @@ class Account(db.Model):
 	username = db.Column(db.String(63), unique=True, nullable=False)
 	password = db.Column(db.String(255), nullable=False)
 	role = db.Column(db.Integer, nullable=False)
-	created_on = db.Column(db.DateTime(timezone=True), default=db.func.current_timestamp(), nullable=False)
-	last_login = db.Column(db.DateTime(timezone=True), onupdate=db.func.current_timestamp()) #onupdate for safekeeping
+	created_on = db.Column(db.DateTime(timezone=False), default=db.func.current_timestamp(), nullable=False)
+	last_login = db.Column(db.DateTime(timezone=False), onupdate=db.func.current_timestamp())
 
 	def __init__(self, email, username, password) -> None:
 		self.email = email
 		self.username
 		self.password = password
 		self.role = 1
+
+	def get_id(self):
+		return self.user_id
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def is_authenticated(self):
+		return True
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,6 +1,31 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
 from application import app
+from application.auth.models import Account
+from application.auth.forms import LoginForm
 
 @app.route('/profile')
 def profile():
 	return render_template('profile.html')
+
+#leaving methods undefined likely defaults exclusively to GET
+@app.route('/login')
+def open_loginform():
+	form = LoginForm()
+
+	return render_template('login_form.html', loginform = form)
+
+@app.route('/login', methods = ['POST'])
+def authenticate_login():
+	form = LoginForm(request.form)
+	# mahdolliset validoinnit
+	print('Tiedot ', request.form, request.form['username'])
+
+	user = Account.query.filter_by(username=form.username.data, password=form.password.data).first()
+	if not user:
+		return render_template('login_form.html', loginform = form,
+								error = 'Invalid credentials')
+
+
+	print("Käyttäjä " + user.username + " tunnistettiin")
+	return redirect( url_for('index') )
