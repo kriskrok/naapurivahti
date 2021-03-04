516bcfe85771a95bc7b1ea21ac0df110a08d5286
kriskrok <25787775+kriskrok@users.noreply.github.com>
2021-03-04 18:11:08 +0200

    Moves some of the startup clutter from application/__init__.py to application/config.py
    - ENV variables now reside in Config class
    - Config is now offered via flask.config.from_object() using the
      aforementioned class instance
    - python-dotenv is still used to read env config variables

--- application/__init__.py
+++ application/__init__.py
@@ -1,26 +1,28 @@
 ### Behold all that will be initialized upon startup
+
 import os, logging, sys
 from flask import Flask
 from flask_login import LoginManager, login_manager
 from dotenv import load_dotenv
 
-app = Flask(__name__)
-
-load_dotenv() #TODO:// Find out why this is required to be invoked
-
 from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv() 
+
+app = Flask(__name__)
 
 if os.environ.get('HEROKU'):
     print('HEROKU enviroment variable found: ', os.environ.get('HEROKU'))
+    app.config.from_object('config.HerokuConfig')
+else:
+    print('Could not find HEROKU enviroment variable')
+    app.config.from_object('application.config.DevConfig')
+    print('Todays session lottery is won by:', os.getenv('SESSION_KEY')[:5], '... \ncongratulations to all the winners!')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
-    # Set SQLAlchemy to print all SQL-queries
-app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = os.getenv('SESSION_KEY')
-print('Todays session lottery is won by:', os.environ.get('SESSION_KEY'))
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+if (True): #TODO:// Conjure a nifty debug variable 
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
 
 # db object for all our ORM needs
 db = SQLAlchemy(app)
@@ -39,8 +41,6 @@ from application.shift import views
 
 # Login functionality
 from application.auth.models import Account
-#from os import urandom
-#app.config["SECRET_KEY"] = urandom(32)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -53,8 +53,6 @@ def load_user(user_id):
 
 
 try:
-    pass
-#    db.create_all()
+    db.create_all()
 except:
     pass
-db.create_all()
---
+++ application/config.py
@@ -0,0 +1,35 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+rootdir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(rootdir, '.env'))
+
+class Config:
+	"""Base config."""
+	SECRET_KEY = environ.get('SESSION_KEY')
+	#SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
+	STATIC_FOLDER = 'static'
+	TEMPLATES_FOLDER = 'templates'
+
+class HerokuConfig(Config):	
+	SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+	### Remove these as end draws near
+	SQLALCHEMY_ECHO = True	# Set SQLAlchemy to print all SQL-queries
+	SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class LocalProdConfig(Config):
+	"""Prod is ran on Gunicorn, but can't hurt to roleplay production
+	circumstances every now and then. Do note, this uses DEV database URI"""
+	FLASK_ENV = 'production'
+	DEBUG = False
+	TESTING = False
+	SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+
+
+class DevConfig(Config):
+	FLASK_ENV = 'development'
+	DEBUG = True
+	TESTING = True			#Propagates exceptions
+	SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+	SQLALCHEMY_ECHO = True	# Set SQLAlchemy to print all SQL-queries
+	SQLALCHEMY_TRACK_MODIFICATIONS = True
